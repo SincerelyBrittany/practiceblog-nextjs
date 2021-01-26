f82b3c39e953d417dbcfdf848f858213f12d9462
SincerelyBrittany <sincerelybr@gmail.com>
2021-01-25 19:33:58 -0500

    adds layout for css

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -3,16 +3,37 @@
 // }
 
 import Link from "next/link";
+import Head from "next/head";
+// export default function FirstPost() {
+//   return (
+//     <>
+//       <Head>
+//         <title>First Post</title>
+//       </Head>
+//       <h1>First Post</h1>
+//       <h2>
+//         <Link href="/">
+//           <a>Back to home</a>
+//         </Link>
+//       </h2>
+//     </>
+//   );
+// }
+
+import Layout from "../../components/layout";
 
 export default function FirstPost() {
   return (
-    <>
+    <Layout>
+      <Head>
+        <title>First Post</title>
+      </Head>
       <h1>First Post</h1>
       <h2>
         <Link href="/">
           <a>Back to home</a>
         </Link>
       </h2>
-    </>
+    </Layout>
   );
 }
