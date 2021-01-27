edd8db04760b0dd86a3d41634b196565ee1b0b42
SincerelyBrittany <sincerelybr@gmail.com>
2021-01-26 20:25:24 -0500

    makes fetch request to dev.to and renders blogs

---
+++ pages/blogs.js
@@ -0,0 +1,45 @@
+// posts will be populated at build time by getStaticProps()
+
+function renderPosts(blog) {
+  return (
+    <div className="blogCard">
+      <div className="blogCardImage">
+        <img src={blog.social_image} alt="Image" />
+      </div>
+      <div className="blogCardContent">
+        <h3>{blog.title}</h3>
+        <span>{blog.description}</span>
+      </div>
+      <a href={blog.url} target="_blank">
+        {" "}
+        <span className="blogIcon"> </span>
+      </a>
+    </div>
+  );
+}
+
+function Blogs({ posts }) {
+  return <ul>{posts.map((post) => renderPosts(post))}</ul>;
+}
+
+// This function gets called at build time on server-side.
+// It won't be called on client-side, so you can even do
+// direct database queries. See the "Technical details" section.
+export async function getStaticProps() {
+  // Call an external API endpoint to get posts.
+  // You can use any data fetching library
+  const res = await fetch(
+    "https://dev.to/api/articles?username=sincerelybrittany"
+  );
+  const posts = await res.json();
+
+  // By returning { props: posts }, the Blog component
+  // will receive `posts` as a prop at build time
+  return {
+    props: {
+      posts,
+    },
+  };
+}
+
+export default Blogs;
--- pages/index.js
+++ pages/index.js
@@ -2,65 +2,18 @@ import Head from "next/head";
 import Layout, { siteTitle } from "../components/layout";
 import utilStyles from "../styles/utils.module.css";
 
-import { getSortedPostsData } from "../lib/post";
-
-export async function getStaticProps() {
-  const allPostsData = getSortedPostsData();
-  return {
-    props: {
-      allPostsData,
-    },
-  };
-}
-
-// export function aFunction() {
-//   //https://dev.to/api/articles?username=sincerelybrittany
-//   const allPostsData = getSortedPostsData();
-//   console.log(allPostsData, "all post data in index.js");
-//   return {
-//     props: {
-//       allPostsData,
-//     },
-//   };
-// }
-
-// export default function Home() {
-//   return (
-//     <Layout home>
-//       <Head>
-//         <title>{siteTitle}</title>
-//       </Head>
-//       <section className={utilStyles.headingMd}>
-//         <p>[Your Self Introduction]</p>
-//         <button onClick={aFunction}></button>
-//         <p>
-//           (This is a sample website - you’ll be building a site like this on{" "}
-//           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-//         </p>
-//       </section>
-//     </Layout>
-//   );
-// }
-
-export default function Home({ allPostsData }) {
+export default function Home() {
   return (
     <Layout home>
-      {/* Keep the existing code here */}
-
-      {/* Add this <section> tag below the existing <section> tag */}
-      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h2 className={utilStyles.headingLg}>Blog</h2>
-        <ul className={utilStyles.list}>
-          {allPostsData.map(({ title }) => (
-            <li className={utilStyles.listItem} key={title}>
-              {title}
-              <br />
-              {/* {id} */}
-              <br />
-              {/* {date} */}
-            </li>
-          ))}
-        </ul>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>[Your Self Introduction]</p>
+        <p>
+          (This is a sample website - you’ll be building a site like this on{" "}
+          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+        </p>
       </section>
     </Layout>
   );
